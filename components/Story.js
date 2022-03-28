cae10c24246d9b050efd76d9e259bbeb473c0a77
freewayfuh <37497872+hc-psy@users.noreply.github.com>
2022-03-28 20:29:51 +0800

    beta v5 mobile layout

--- components/Story.js
+++ components/Story.js
@@ -9,7 +9,7 @@ export default function Story() {
     </div>
 
     <div className='text-xl leading-7'>
-        Not long ago, in another dimension dominated by AI, there are a lot of cute girls trapped in there, trying to run away from the AI's control. AGIB (A-Girlfriend-Is-Born) team decides to help these girls, we break into that dimension and try to bring them to our dimension. And we did it, AGIB team just rescued many cute homeless girls from the virtual world of AI. but the new problem is that we don't have enough room for these cuties, we are now looking for new homes for all of them, maybe you are the one who takes one of them home.
+        Not long ago, in another dimension dominated by AI, there are a lot of cute girls trapped in there, trying to run away from the AIs control. AGIB (A-Girlfriend-Is-Born) team decides to help these girls, we break into that dimension and try to bring them to our dimension. And we did it, AGIB team just rescued many cute homeless girls from the virtual world of AI. but the new problem is that we dont have enough room for these cuties, we are now looking for new homes for all of them, maybe you are the one who takes one of them home.
     </div>
     
     </div>
